af16c080987814b24966c3986b496acf1435ee6d
Curso <Curso@IPARTEKAULA.COM>
2020-01-17 09:44:40 +0100

    app angular inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
+/**
+ * componente principal que se carga al arrancar la app
+ * 
+ * @Component: así se declara 1 componente
+ * SELECTOR: nombre de la etiqueta para inyectar componente en HTML
+ * templateUrl: vista del HTML
+ * styleUrls: estilos para la vista formato scss
+ */
+@Component({ 
+  selector: 'app-root', 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularApp';
+  //variable
+  title = 'angularApp'; //la podemos pintar en app.component.html con {{title}}
+
+  subtitle = 'subtitulo de angularApp';
 }
+
