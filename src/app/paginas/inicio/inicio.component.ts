b65e89b953345e90049f8463ce3bbb309ecb4610
Curso <Curso@IPARTEKAULA.COM>
2020-02-04 12:31:08 +0100

    ej lujo de info de la vista al modelo y viceversa

--- src/app/paginas/inicio/inicio.component.ts
+++ src/app/paginas/inicio/inicio.component.ts
@@ -9,16 +9,24 @@ import { PokemonService } from 'src/app/services/pokemon.service';
 export class InicioComponent implements OnInit {
 
   pokemon: any; //creamos una variable pokemon de cualquier tipo
+  title: string;
+  numeroClicks: number;
 
   constructor( private pokemonService: PokemonService ) {
     console.trace('InicioComponent constructor');
+
+    //inicializamos variables:
     this.pokemon = {};
+    this.title = "Welcome to Angular";
+    this.numeroClicks = 0;
+
   } // fin constructor
 
   ngOnInit() {
 
     console.trace('InicioComponent ngOnInit');
 
+    //pokemonService:
     this.pokemonService.getPokemon('charmander').subscribe(
       data => {
         console.debug('petición ok %o' , data);
@@ -32,4 +40,14 @@ export class InicioComponent implements OnInit {
 
   } // fin ngOnInit
 
-}
+
+  //contador número de clicks para ver el Flujo de información de la vista al modelo y modelo a vista:
+  contarClicks() {
+
+    console.trace('Contar Clicks');
+    this.numeroClicks++;
+
+  } //fin contarClicks
+
+
+} //fin InicioComponent
