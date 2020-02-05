241e900ad446cf06bf118724b7e9ba4aec24c298
Curso <Curso@IPARTEKAULA.COM>
2020-02-05 13:56:07 +0100

    directiva appHello con más ejemplos

--- src/app/directivas/hello.directive.ts
+++ src/app/directivas/hello.directive.ts
@@ -6,7 +6,9 @@ import { Directive, HostListener, ElementRef, Input } from '@angular/core';
 export class HelloDirective {
 
   //Input. pasamos a la directiva un parámetro para decirle el color de fondo que queremos:
-  @Input() appHello:string;
+  //@Input() appHello:string;
+  @Input() colorTexto: string;
+  @Input() colorFondo: string;
 
   constructor(private element: ElementRef) { 
     console.trace('HelloDirective constructor');
@@ -19,17 +21,23 @@ export class HelloDirective {
 
     //1) cuando pasamos el ratón por el elemento que tenga la directiva, se pintará de color rojo
     //Para esto hay que pasárselo al constructor:
-    this.element.nativeElement.style.backgroundColor = "red"; 
+    //this.element.nativeElement.style.backgroundColor = "red"; 
 
     //2) Input. pasamos a la directiva un parámetro para decirle el color de fondo que queremos:
-    this.element.nativeElement.style.backgroundColor = this.appHello;
+    //this.element.nativeElement.style.backgroundColor = this.appHello;
+    
+    //3) podemos usar varios parámetros:
+    this.element.nativeElement.style.backgroundColor = this.colorFondo;
+    this.element.nativeElement.style.color = this.colorTexto;
 
   } //fin publiconMouseEnter
  
   @HostListener('mouseleave')
   publiconMouseLeave(){
     console.trace('HelloDirective publiconMouseLeave');
-    this.element.nativeElement.style.backgroundColor = "green";
+    //al quitar el ratón dejamos el formato normal:
+    this.element.nativeElement.style.backgroundColor = 'white';
+    this.element.nativeElement.style.color = 'black';
   } //fin publiconMouseLeave
 
 } //fin HelloDirective
