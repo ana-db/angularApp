b52820c5028e560101b80a38137b853643e76d36
Curso <Curso@IPARTEKAULA.COM>
2020-02-06 10:40:57 +0100

    nav en array

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RUTAS } from '../../rutas';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  rutas: Array<any>;
+
+  constructor() { 
+    console.trace('NavbarComponent constructor');
+    this.rutas = RUTAS;
+  } //fin constructor
 
   ngOnInit() {
-  }
+    console.trace('NavbarComponent ngOnInit');
+  } //fin ngOnInit
 
 }
---
+++ src/app/rutas.ts
@@ -0,0 +1,52 @@
+export const RUTAS = 
+
+[
+    {
+        'ruta' : '/',
+        'nombre' : 'Inicio',
+        'icono' : 'fas fa-home'
+    },
+
+    {
+        'ruta' : '/productos',
+        'nombre' : 'Productos',
+        'icono' : 'fas fa-shopping-cart'
+    },
+
+    {
+        'ruta' : '/arrays',
+        'nombre' : 'Arrays',
+        'icono' : 'fas fa-ellipsis-h'
+    },
+
+    {
+        'ruta' : '/pokemon',
+        'nombre' : 'Ej. Clase y Rest (Pokemon)',
+        'icono' : 'fas fa-paw'
+    },
+
+    {
+        'ruta' : '/estilos',
+        'nombre' : 'Sass',
+        'icono' : 'fab fa-sass'
+    },
+
+    {
+        'ruta' : '/juego',
+        'nombre' : 'Juego',
+        'icono' : 'fas fa-mouse'
+    },
+
+    {
+        'ruta' : '/directivas',
+        'nombre' : 'Directivas',
+        'icono' : 'fas fa-directions'
+    },
+
+    {
+        'ruta' : '/filtros',
+        'nombre' : 'Filtros',
+        'icono' : 'fas fa-sort-amount-down'
+    }
+
+];
