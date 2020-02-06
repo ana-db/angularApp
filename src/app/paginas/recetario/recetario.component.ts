b1a12c29cbe80608118131fd90f36fa98c54e654
Curso <Curso@IPARTEKAULA.COM>
2020-02-06 13:57:57 +0100

    recetas

--- src/app/paginas/recetario/recetario.component.ts
+++ src/app/paginas/recetario/recetario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RECETAS } from '../../recetas';
 
 @Component({
   selector: 'app-recetario',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecetarioComponent implements OnInit {
 
-  constructor() { }
+  recetas : Array<any>;
+  rSeleccionada: Array<any>; //-- 
+
+  constructor() { 
+    console.trace('RecetarioComponent constructor');
+    this.recetas = RECETAS;
+    this.rSeleccionada = this.recetas[0]; 
+    /*this.rSeleccionada = this.recetas[0]; --> tenemos que hacer esta asignación en el constructor 
+    porque si no, tenía un error "TypeError: Cannot read property '0' of undefined" 
+    porque no había declarado rSeleccionada en ningún sitio ni la había inicializado
+    */
+  } //fin constructor
+
 
   ngOnInit() {
+    console.trace('RecetarioComponent ngOnInit');
+  } // fin constructor
+
+
+  //función para seleccionar recetas y sacar su detalle: 
+  seleccionarReceta = function(receta){
+    console.log('Hemos hecho click en la receta %o', receta ); 
+    this.rSeleccionada = receta;
   }
+  
 
 }
