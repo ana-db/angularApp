0809c79004f1147d67a68c6c4ba2ef7f4a934f7f
Curso <Curso@IPARTEKAULA.COM>
2020-02-07 09:50:25 +0100

    filtro por nombre receta, cocinero o ingrediente

--- src/app/pipes/buscador-recetas.pipe.ts
+++ src/app/pipes/buscador-recetas.pipe.ts
@@ -5,12 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class BuscadorRecetasPipe implements PipeTransform {
 
+  /**
+   * 
+   * @param recetas Array<any> con recetas
+   * @param checkGluten dato boolean para buscar recetas con o sin gluten
+   * @param busqueda cadena de caracteres con el nombre de la receta, cocinero o ingredientes que buscamos
+   */
   transform(recetas: any, checkGluten : boolean, busqueda : string ): any {
 
     console.trace('---------- empieza pipe ----------');
-    console.debug(recetas); //contenido del array
-    console.debug(checkGluten); //elementos por los que filtramos: recetas con o sin gluten
-    console.debug(busqueda); //elementos por los que filtramos: nombre de la receta
+    console.debug('recetas', recetas); //contenido del array
+    console.debug('checkGluten', checkGluten); //elementos por los que filtramos: recetas con o sin gluten
+    console.debug('busqueda', busqueda); //elementos por los que filtramos: nombre de la receta, cocinero o ingredientes
     console.trace('---------- termina pipe ----------');
 
     let resultado = recetas;
@@ -20,19 +26,20 @@ export class BuscadorRecetasPipe implements PipeTransform {
       resultado = resultado.filter( (el) => el.isGlutenFree );
     }
 
-    /*
-    if( busqueda && '' !== busqueda.trim() ){ //con este if evitamos que cuando entremos en la pagina, tengamos la lista vacía (undefined)
-      //1) filtramos por NOMBRE:
-      busqueda = busqueda.toUpperCase();
+    //2) filtrar por NOMBRE, COCINERO o INGREDIENTES de la receta:
+    if( busqueda && '' !== busqueda ){ 
 
-      const resultado = recetas.filter( (el) => {
-        console.debug(el);
-        const nombre = el.Nombre.toUpperCase(); //pasamos el nombre a mayúsculas, porque incudes() distingue entre mayus y minus
-        return nombre.includes(busqueda);  //includes indica si la palabra en "busqueda" está en "nombre"
+      busqueda = busqueda.toLowerCase();
+
+      resultado = resultado.filter( (el) => {
+        const ingredientes = el.ingredientes.reduce( (c,p) => c + p, '' ); //cogemos el array con todos los ingredientes y los ingredientes y los reducimos a 1 string
+        const encontrar = (el.nombre + el.cocinero + ingredientes).toLowerCase(); //concatenamos el nombre de la receta, del cocinero y el string con los igredientes y lo pasamos a minúsculas
+        console.debug('encontrar', encontrar);
+        return encontrar.includes(busqueda);  //includes indica si la cadena que tenemos guardada en "busqueda" está en "encontrar", si es asi, lo guarda en resultado para mostrarlo por pantalla
       });
 
     }
-    */
+
     return resultado;
     
   } //fin transform
